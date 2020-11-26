a3bc9d62985d1ed7e8b63c30b1891385ddc3a9e1
Jeff Golden <gjgolden08@gmail.com>
2020-11-26 20:37:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), updated_list|
     value.each do |color, name|
       if !updated_list[name]
-        updated_list << 
+        updated_list[name] = {}
 binding.pry 
   end
 end
