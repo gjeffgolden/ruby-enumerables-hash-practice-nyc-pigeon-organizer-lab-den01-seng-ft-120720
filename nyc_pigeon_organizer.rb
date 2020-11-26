02c915544cd43bbe6a964ecb1df625eb6a9dae17
Jeff Golden <gjgolden08@gmail.com>
2020-11-26 20:31:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,9 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), updated_list|
-    
+    value.each do |color, name|
+      binding.pry 
+  end
   
 end
